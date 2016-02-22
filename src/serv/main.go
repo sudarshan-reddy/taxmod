9a13b1a652aa6c4888379452cc7933aa71af9667
Sudarshan Reddy <sudar.theone@gmail.com>
2016-02-22 05:34:33 +0000

    changing some stuff for heroku uploads

--- src/serv/main.go
+++ src/serv/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"os"
+	"fmt"
 	//"taxmain"
 )
 
@@ -10,6 +11,10 @@ import (
 func main(){
 	fs := http.FileServer(http.Dir("app/public"))
 	port := os.Getenv("PORT")
+	fmt.Println("Port being listened to: " , port)	
+	if port == "" {
+		port = "5000"
+	}
 	http.Handle("/" , fs)
 	http.ListenAndServe(":" + port, nil)
 }
